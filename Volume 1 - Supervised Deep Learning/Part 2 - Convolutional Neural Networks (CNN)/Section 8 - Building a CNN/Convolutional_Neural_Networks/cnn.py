979bc4bb4a4a421cdc35ce9fb521311729973a0e
Phil Brosgol <phil.brosgol@aurotfp.com>
2019-12-09 14:28:07 -0500

    compiling

--- Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Convolutional_Neural_Networks/cnn.py
@@ -13,7 +13,25 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+# initialize the CNN
+classifier = Sequential()
+#Step 1 - Convolution
+# NOTE: When using tensorflow backend, the order for input_shape is row/col/channels, but for theano it's reversed
+classifier.add(Convolution2D(nb_filter = 32, nb_row = 3, nb_col = 3, 
+                             input_shape = (64, 64, 3), 
+                             activation = 'relu'))
+#Step 2 - max pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+#Step 3 - flatten layers
+classifier.add(Flatten())
+#Step 4 - full connection (implement the classic ANN)
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
-
+# Compiling the CNN
+# If we had multiple categories instead of binary, we would use categorical_crossentropy
+classifier.compile(optimizer = 'adam',
+                   loss = 'binary_crossentropy',
+                   metrics = )
 
 
