bd76a1220b1b27a21961ef06dc1953999a933a76
Phil Brosgol <bluremi@gmail.com>
2019-12-12 14:46:21 -0500

    adding the layers

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks/rnn.py
@@ -22,11 +22,33 @@ training_set_scaled = sc.fit_transform(training_set)
 X_train = []
 y_train = []
 for i in range(60,training_set_scaled.size - 1):
-    prevHist = []
-    X_train.append(training_set_scaled[i-60:i])
-    y_train.append(training_set_scaled[i])
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+# Add a dimension to the numpy array using Reshaping function
+# The new dimension indexes the model inputs. In our model we only have a single input (open price) so there's just one dimension
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 """ Part 2 - Building the RNN """
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor = Sequential()
+
+# Add four LSTM layers with Dropout regularization to avoid over-fitting
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(rate = 0.2))
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(rate = 0.2))
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(rate = 0.2))
+regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(Dropout(rate = 0.2))
+
 
 
 """ Part 3 - Make predictions and visualize the results """
