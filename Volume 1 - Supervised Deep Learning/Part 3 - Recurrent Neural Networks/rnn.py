742a67fa10f9cc8072b30fbc5adf361b18082770
Phil Brosgol <bluremi@gmail.com>
2019-12-12 16:08:50 -0500

    generated predictions

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks/rnn.py
@@ -46,9 +46,42 @@ regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(rate = 0.2))
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(rate = 0.2))
-regressor.add(LSTM(units = 50, return_sequences = False))
+regressor.add(LSTM(units = 50))
 regressor.add(Dropout(rate = 0.2))
 
+# Add output layer
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+regressor.fit(x = X_train, y = y_train, batch_size = 32, epochs = 100)
 
 
 """ Part 3 - Make predictions and visualize the results """
+# Get the real observed values for January 2017
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Generate some predicted stock prices for January 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range (60, inputs.size):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+
+
+
+
+
+
+
+
+
